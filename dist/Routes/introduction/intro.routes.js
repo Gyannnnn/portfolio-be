4838bca05215f6496560000fed5ebff4bd28032f
Gyan <hi.gyanaranjanpatra@gmail.com>
2025-08-28 10:22:19 +0530

    feat: add authentication middleware to update endpoint

--- dist/Routes/introduction/intro.routes.js
+++ dist/Routes/introduction/intro.routes.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const introductionRouter = (0, express_1.Router)();
 const intro_controller_1 = require("../../controller/introduction/intro.controller");
+const auth_middleware_1 = require("../../middleware/auth.middleware");
 introductionRouter.get("/get", intro_controller_1.getIntro);
-introductionRouter.put("/update", intro_controller_1.updateIntro);
+introductionRouter.put("/update", auth_middleware_1.authMiddleware, intro_controller_1.updateIntro);
 exports.default = introductionRouter;
--- src/Routes/introduction/intro.routes.ts
+++ src/Routes/introduction/intro.routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 const introductionRouter = Router();
 
 import { getIntro, updateIntro } from "../../controller/introduction/intro.controller";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 
 introductionRouter.get("/get", getIntro);
-introductionRouter.put("/update",updateIntro);
+introductionRouter.put("/update",authMiddleware,updateIntro);
 
 export default introductionRouter;
